9a7ad3f6b2248df9f6c87fb2cd2c353d19e7cfcb
pedrocorrea2 <70420813+pedrocorrea2@users.noreply.github.com>
2025-08-28 16:20:50 -0400

    feat: add fixed top navigation

---
+++ components/Offices.jsx
@@ -0,0 +1,22 @@
+export default function Offices() {
+  return (
+    <section id="oficinas" className="offices">
+      <div className="container">
+        <h2>Nuestras Oficinas</h2>
+        <div className="offices-grid">
+          <div>
+            <p><strong>Antofagasta</strong><br />Arturo Prat 461 Of. 1802<br />55 2251 340</p>
+            <p><strong>Copiapó</strong><br />clezcano@acfcapital.cl<br />2 23389400</p>
+            <p><strong>Santiago, Casa Matriz</strong><br />Av. El Bosque Norte 0177 Of. 802<br />Las Condes<br />22338 9400</p>
+            <p><strong>Santiago, Providencia</strong><br />Av. Providencia 2653 Of. 1102<br />Providencia<br />22520 6000 - 22233 2469</p>
+          </div>
+          <div>
+            <p><strong>Viña del Mar</strong><br />Av. Nueva libertad 1405 Of.1306<br />32 2690 759</p>
+            <p><strong>Rancagua</strong><br />eguajardo@acfcapital.cl<br />2 23389400</p>
+            <p><strong>Puerto Montt</strong><br />Benavente 550 Of. 503<br />65 2348 910</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -3,9 +3,11 @@ import Hero from '../components/Hero';
 import Services from '../components/Services';
 import Stats from '../components/Stats';
 import HowItWorks from '../components/HowItWorks';
-import WorkWithUs from '../components/WorkWithUs';
+import FactoringCalculator from '../components/FactoringCalculator';
 import ContactForm from '../components/ContactForm';
 import Footer from '../components/Footer';
+import Offices from '../components/Offices';
+import WhatsAppButton from '../components/WhatsAppButton';
 
 export default function Home() {
   return (
@@ -15,9 +17,11 @@ export default function Home() {
       <Services />
       <Stats />
       <HowItWorks />
-      <WorkWithUs />
+      <FactoringCalculator />
+      <Offices />
       <ContactForm />
       <Footer />
+      <WhatsAppButton />
     </>
   );
 }
